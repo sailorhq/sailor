e05de81d3f162353bee03f3064e068a50457c364
codekidX <ckrackerx15@gmail.com>
2025-06-21 17:42:29 +0530

    add rule handler where you can patch new rules on top of first rule

--- cmd/sailor/handlers/rules.go
+++ cmd/sailor/handlers/rules.go
@@ -1,7 +1,58 @@
 package handlers
 
-import "net/http"
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+
+	bolt "go.etcd.io/bbolt"
+)
 
 func RuleHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPatch {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	ns := r.URL.Query().Get("ns")
+	app := r.URL.Query().Get("app")
+	_ = r.URL.Query().Get("key")
+
+	enc := json.NewEncoder(w)
+
+	if ns == "" || app == "" {
+		enc.Encode(ResponseMessage{Message: "namespace or app is empty"})
+		return
+	}
+
+	dbpath := fmt.Sprintf("./configs/%s-%s.db", ns, app)
+	if f, _ := os.Stat(dbpath); f == nil {
+		enc.Encode(ResponseMessage{Message: "no such app in this namespace"})
+		return
+	}
+
+	db, err := bolt.Open(dbpath, 0600, nil)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	b, _ := io.ReadAll(r.Body)
+	defer r.Body.Close()
+
+	if ok := json.Valid(b); !ok {
+		enc.Encode(ResponseMessage{Message: "invalid json for rules!"})
+		return
+	}
+
+	if err = db.Update(func(tx *bolt.Tx) error {
+		metaBucket := tx.Bucket([]byte(BUCKET_META))
+		return metaBucket.Put([]byte(KEY_RULES), b)
+	}); err != nil {
+		enc.Encode(ResponseMessage{Message: err.Error()})
+		return
+	}
 
 }
--- cmd/sailor/main.go
+++ cmd/sailor/main.go
@@ -11,6 +11,7 @@ func main() {
 	http.HandleFunc("/update", handlers.UpdateAppMetaHandler)
 	http.HandleFunc("/config", handlers.ConfigHandler)
 	http.HandleFunc("/deploy", handlers.DeployHandler)
+	http.HandleFunc("/rules", handlers.RuleHandler)
 
 	http.ListenAndServe(":7766", nil)
 }
