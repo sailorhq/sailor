655273c3d57196cdc4792712d5e0bb4aeb1ce283
codekidX <ckrackerx15@gmail.com>
2025-07-24 23:19:55 +0530

    refactor: disable logic inside version API

--- cmd/sailor/handlers/version.go
+++ cmd/sailor/handlers/version.go
@@ -3,38 +3,31 @@ package handlers
 import (
 	"fmt"
 	"net/http"
-
-	bolt "go.etcd.io/bbolt"
 )
 
+// VersionHandler ...
+// TODO :: need to create version API for efficient pulls
 func (sc *SailorCore) VersionHandler(w http.ResponseWriter, r *http.Request) {
-	params, err := sc.extractSailorParams(r)
-	if err != nil {
-		// TODO: log here!
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
+	// key := fmt.Sprintf("%s-%s", params.Ns, params.App)
+	// if version, ok := sc.versions[key]; ok {
+	// 	fmt.Fprint(w, version)
+	// 	return
+	// }
 
-	key := fmt.Sprintf("%s-%s", params.Ns, params.App)
-	if version, ok := sc.versions[key]; ok {
-		fmt.Fprint(w, version)
-		return
-	}
-
-	db, err := sc.getDBConn(params)
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
+	// db, err := sc.getDBConn(params)
+	// if err != nil {
+	// 	w.WriteHeader(http.StatusInternalServerError)
+	// 	return
+	// }
 
 	var ver string
-	db.View(func(tx *bolt.Tx) error {
-		// fetch current deployed version ...
-		metaBucket := tx.Bucket([]byte(BUCKET_META))
-		ver = string(metaBucket.Get([]byte(KEY_DEPLOYED_VERSION)))
+	// db.View(func(tx *bolt.Tx) error {
+	// 	// fetch current deployed version ...
+	// 	metaBucket := tx.Bucket([]byte(BUCKET_META))
+	// 	ver = string(metaBucket.Get([]byte(KEY_DEPLOYED_VERSION)))
 
-		return nil
-	})
+	// 	return nil
+	// })
 
 	fmt.Fprint(w, ver)
 }
