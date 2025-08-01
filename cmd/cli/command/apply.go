5c8e652b791e6ffbfe9919bbb450f74ad0062e99
codekidX <ckrackerx15@gmail.com>
2025-08-01 17:53:27 +0530

    feat: apply command
    apply command is used to load sailor manifest from Sailor Core Server and apply environment to work on as well

--- cmd/cli/command/apply.go
+++ cmd/cli/command/apply.go
@@ -1,34 +1,105 @@
 package command
 
 import (
+	"encoding/json"
 	"errors"
 	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
 
+	v1 "github.com/codekidx/sailor/pkg/core/v1"
 	"github.com/spf13/cobra"
 )
 
-func ApplyCommand() *cobra.Command {
+func ApplyCommand(cfg *CLIConfig) *cobra.Command {
 	var file string
-	var url string
+	var host string
 	applyCmd := &cobra.Command{
 		Use:   "apply",
 		Short: "Helps configure your sailor environment",
 		RunE: func(cmd *cobra.Command, args []string) error {
-			if url == "" && file == "" && len(args) == 0 {
+			if host == "" && file == "" && len(args) == 0 {
 				return errors.New("flags or env as argument is required")
 			}
 
-			if url == "" && file == "" {
+			// TODO :: separate logic into nice functions!
+
+			if host == "" && file == "" {
 				env := args[0]
-				// TODO :: we need to check if sailor manifest file has this environment
-				fmt.Println("selected: ", env)
+				for _, envConfig := range cfg.Manifest.Envs {
+					if strings.EqualFold(env, envConfig.Name) {
+						if err := os.WriteFile(filepath.Join(cfg.SailorRoot, "env"), []byte(envConfig.Name), 0655); err != nil {
+							return err
+						}
+						fmt.Printf("switched to %s\n", envConfig.Name)
+						return nil
+					}
+				}
+
+				fmt.Println("no environment named: ", env)
+
+				return nil
+			}
+
+			if host != "" {
+				manifestURL := fmt.Sprintf("%s/api/v1/setting/manifest", host)
+				resp, err := http.Get(manifestURL)
+				if err != nil {
+					return err
+				}
+
+				if resp.StatusCode != 200 {
+					return fmt.Errorf("error while fetching manifest, status: %d", resp.StatusCode)
+				}
+
+				defer resp.Body.Close()
+				b, err := io.ReadAll(resp.Body)
+				if err != nil {
+					return err
+				}
+
+				var manifest v1.SailorManifest
+				if err := json.Unmarshal(b, &manifest); err != nil {
+					return err
+				}
+
+				// since it is parsable as sailor manifest, we save it!
+				if err := os.WriteFile(filepath.Join(cfg.SailorRoot, "config"), b, 0755); err != nil {
+					return err
+				}
+
+				fmt.Println("sailor manifest loaded!")
+				return nil
+			}
+
+			if file != "" {
+				b, err := os.ReadFile(file)
+				if err != nil {
+					return err
+				}
+
+				var manifest v1.SailorManifest
+				if err := json.Unmarshal(b, &manifest); err != nil {
+					return err
+				}
+
+				// since it is parsable as sailor manifest, we save it!
+				if err := os.WriteFile(filepath.Join(cfg.SailorRoot, "config"), b, 0755); err != nil {
+					return err
+				}
+
+				fmt.Println("sailor manifest loaded!")
+				return nil
 			}
 
 			return nil
 		},
 	}
 
-	applyCmd.Flags().StringVarP(&url, "url", "", "", "configuration fetch url")
+	applyCmd.Flags().StringVarP(&host, "host", "", "", "host where sailor is hosted")
 	applyCmd.Flags().StringVarP(&file, "file", "f", "", "configuration file")
 
 	return applyCmd
